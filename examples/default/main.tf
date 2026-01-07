75993c34acbb89f921672155456cbc663ab92259
azurebear445 <awsatlnta@gmail.com>
2026-01-07 16:58:21 -0500

    Add files via upload

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,10 +1,17 @@
+# Create Resource Group for testing
+resource "azurerm_resource_group" "this" {
+  name     = "rg-storage-example"
+  location = "centralus"
+}
+
 module "storage_account" {
-  source = "../../"
+  # source = "../../"
+  source = "git::ssh://git@gitlab.com/webster-bank/team-cloud-engineering/system-azure_terraform_modules/terraform-azurerm-storage_account.git?ref=CLOUD-XXXX"
 
   namespace           = var.namespace
   environment         = var.environment
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
   tags                = var.tags
 }
 
